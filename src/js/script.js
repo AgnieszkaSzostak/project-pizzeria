da9cecc6c869559ba0286312ac017494173586cc
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-09-13 18:25:04 +0200

    Add loop in app.initMenu method && fix class Product

--- src/js/script.js
+++ src/js/script.js
@@ -52,16 +52,27 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
   class Product{
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
 
       console.log('new Product:', thisProduct);
     }
   }
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
     },
     init: function(){
       const thisApp = this;
@@ -71,6 +82,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
