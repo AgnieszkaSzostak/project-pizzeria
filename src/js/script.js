c30784bf2c5d894223ced691c7c356297a87c405
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-09-13 18:30:00 +0200

    Add renderInMenu method

--- src/js/script.js
+++ src/js/script.js
@@ -56,8 +56,12 @@
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
-
+      thisProduct.renderInMenu();
       console.log('new Product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+
     }
   }
   const app = {
