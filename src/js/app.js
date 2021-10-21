fe3d5ce82971cf5748bc0ef085b464f1cd637149
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-10-21 21:36:46 +0200

    Add initPages function

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,61 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 
-
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    let pageMatchingHash = thisApp.pages[0].id;
+    
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+   
+    thisApp.activatePage(pageMatchingHash);
+    
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thisApp.activatePage with id */
+        thisApp.activatePage(id);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
   initMenu: function(){
     const thisApp = this;
     for(let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
   }, 
+  activatePage: function(pageId){
+    const thisApp = this;
+    /* add class active to matching pages, remove from non matching */
+    for(let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /* add class active to matching links, remove from non matching */
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
   initData: function(){
     const thisApp = this;
 
@@ -24,12 +69,13 @@ const app = {
         thisApp.data.products = parsedResponse; 
         thisApp.initMenu();
       });
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
   init: function(){
     const thisApp = this;
    
+    thisApp.initPages();
     thisApp.initData();
+
   },
   initCart: function(){
     const thisApp = this;
