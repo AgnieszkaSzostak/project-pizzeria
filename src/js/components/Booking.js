a5cfcb16aca4c445fccdc7afbdf66905e7b0e868
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-10-22 19:37:18 +0200

    Add render && initWidgets method in Booking class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,21 +1,29 @@
-import { templates } from '../settings.js';
-import { utils } from '../utils.js';
+import { templates, select } from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 
 class Booking{
   constructor(element){
     const thisBooking = this;
     thisBooking.render(element);
-    // thisBooking.initWidgets();
+    thisBooking.initWidgets();
   }
-  render(elem){
+  render(element){
     const thisBooking = this;
     const generatedHTML = templates.bookingWidget();
     console.log('generatedHTML', generatedHTML);
     thisBooking.dom = {};
-    thisBooking.dom.wrapper = elem;
+    thisBooking.dom.wrapper = element;
     console.log('thisBooking.dom.wrapper', thisBooking.dom.wrapper);
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
-
+    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+  }
+  initWidgets(){
+    const thisBooking = this;
+    thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.peopleAmountWidget.addEventListener('updated',function(){});
+    thisBooking.hoursAmountWidget.addEventListener('updated',function(){});
   }
 }
 export default Booking;
