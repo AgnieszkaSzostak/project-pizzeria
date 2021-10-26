42a1c10ebf93148d3e608747fe39148abf948d2f
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-10-26 14:05:45 +0200

    Add getData method to Booking.js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,13 +1,58 @@
-import { templates, select } from '../settings.js';
+import { templates, select, settings } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
+import {utils} from '../utils.js';
 
 class Booking{
   constructor(element){
     const thisBooking = this;
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
+  }
+  getData(){
+    const thisBooking = this;
+    console.log('thisBooking', thisBooking);
+    
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(new Date()); 
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(utils.addDays(utils.dateToStr(new Date()), settings.datePicker.maxDaysInFuture));
+    
+    const params = {
+      bookings: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    console.log('getData params', params);
+
+    const  urls = {
+      bookings:       settings.db.url + '/' + settings.db.booking 
+                                     + '?' + params.bookings.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event   
+                                     + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + settings.db.event   
+                                     + '?' + params.eventsRepeat.join('&'),
+    };
+
+    fetch(urls.bookings)
+      .then(function(bookingsResponse){
+        return bookingsResponse.json();
+      })
+      .then(function(bookings){
+        console.log('bookings', bookings);
+      });
+    console.log('getData urls', urls);
   }
   render(element){
     const thisBooking = this;
