03d6570dab4c33a26c2dcde90c7cd2be87feaf3e
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-10-28 14:11:07 +0200

    Add Promise.all method in Booking.js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -45,13 +45,24 @@ class Booking{
                                      + '?' + params.eventsRepeat.join('&'),
     };
     
-    fetch(urls.booking)
-      .then(function(bookingsResponse){
-        return bookingsResponse.json();
-      })
-      .then(function(bookings){
-        console.log('bookings', bookings);
-      });
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ]).then(function(allResponses){
+      const bookingsResponse = allResponses[0];
+      const eventsCurrentResponse = allResponses[1];
+      const eventsRepeatResponse = allResponses[2];
+      return Promise.all([
+        bookingsResponse.json(),
+        eventsCurrentResponse.json(),
+        eventsRepeatResponse.json(),
+      ]);
+    }).then(function([bookings, eventsCurrent, eventsRepeat]){
+      console.log('bookings', bookings);
+      console.log('eventsCurrent', eventsCurrent);
+      console.log('eventsRepeat', eventsRepeat);
+    });
     console.log('getData urls', urls);
   }
   render(element){
