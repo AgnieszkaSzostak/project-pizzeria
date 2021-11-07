b2a2cb9b3fcecb06b7898553b5233705b539f97f
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-07 13:55:29 +0100

    Add initLinks & activatePage method in Home.js

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,11 +1,11 @@
-import {templates} from '../settings.js';
-
+import {templates, select, classNames} from '../settings.js'; 
 
 class Home{
   constructor(element){
     const thisHome = this;
     console.log('thisHome', thisHome);
     thisHome.render(element);
+    thisHome.initLinks();
     thisHome.initCarousel();
   }
 
@@ -15,6 +15,37 @@ class Home{
     thisHome.dom = {};
     thisHome.dom.wrapper = element;
     thisHome.dom.wrapper.innerHTML = generatedHTML;
+  }
+  
+  activatePage(pageId){
+    const thisHome = this;
+
+    thisHome.pages = document.querySelector(select.containerOf.pages).children;
+    thisHome.navLinks = document.querySelectorAll(select.nav.links);
+
+    for(let page of thisHome.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId );
+    }
+    for(let link of thisHome.navLinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+
+  }
+  initLinks(){
+    const thisHome = this;
+    
+    thisHome.links = document.querySelectorAll('.link');
+
+    for(let link of thisHome.links){
+      link.addEventListener('click', function(event){
+        event.preventDefault;
+
+        const clickedLink = this;
+        const id = clickedLink.getAttribute('href').replace('#', '');
+
+        thisHome.activatePage(id);
+      });
+    }
   }
 
   initCarousel(){
