13554d3986caff1145493f3b769170e0283b4cd1
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-10 10:26:27 +0100

    Add Procfile and  server.js files  & change url in settings.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -114,7 +114,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     product: 'product',
